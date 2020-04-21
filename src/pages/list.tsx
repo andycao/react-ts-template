552df0ebf445926de79c4a5b5e362faeb5b9ad4f
andy <huayu.cao@pintec.com>
2020-04-21 11:07:09 +0800

    prettier rc

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -2,51 +2,51 @@ import React from "react";
 import ShowTheLocation from "./index";
 
 function List() {
-  return (
-    <div className="myList">
-      <ShowTheLocation />
-      <hr />
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-      <div>content</div>
-    </div>
-  );
+    return (
+        <div className="myList">
+            <ShowTheLocation />
+            <hr />
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+            <div>content</div>
+        </div>
+    );
 }
 
 export default List;
--- src/pages/queryDemo.js
+++ src/pages/queryDemo.js
@@ -3,22 +3,22 @@ import { useQuery } from "../hooks/useQuery";
 import { Link, useRouteMatch } from "react-router-dom";
 
 function QueryDemo() {
-  const query = useQuery();
-  const match = useRouteMatch();
-  console.log(match);
-  return (
-    <div>
-      <ul>
-        <li>
-          <Link to={match.url + "?test=first"}>first</Link>
-        </li>
-        <li>
-          <Link to={match.url + "?test=second"}>second</Link>
-        </li>
-      </ul>
-      <p>test: {query.get("test")}</p>
-    </div>
-  );
+    const query = useQuery();
+    const match = useRouteMatch();
+    console.log(match);
+    return (
+        <div>
+            <ul>
+                <li>
+                    <Link to={match.url + "?test=first"}>first</Link>
+                </li>
+                <li>
+                    <Link to={match.url + "?test=second"}>second</Link>
+                </li>
+            </ul>
+            <p>test: {query.get("test")}</p>
+        </div>
+    );
 }
 
 export default QueryDemo;
