df25f67ecab3f2ea43bb09ca3a1fecdb455ba96f
andy <huayu.cao@pintec.com>
2020-04-22 10:51:14 +0800

    add prettier config

--- src/hooks/useQuery.js
+++ src/hooks/useQuery.js
@@ -1,6 +1,6 @@
 import { useLocation } from "react-router-dom";
 
 function useQuery() {
-  return new URLSearchParams(useLocation().search);
+    return new URLSearchParams(useLocation().search);
 }
 export { useQuery };
